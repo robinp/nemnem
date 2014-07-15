f40ab3ab2c1ff0f52f67e2e8f37fdd902bd54982
Robin Palotai <palotai.robin@gmail.com>
2014-07-15 23:39:15 +0000

    Removed sources from pages branch

--- install-all.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-for pkg in nemnem-lib nemnem-genstatic
-do
-  pushd $pkg
-  cabal install || die "Error"
-  popd
-done
--- prepare-sandbox.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cabal sandbox init
-for pkg in nemnem-genstatic nemnem-lib
-do
-  ln -s $(readlink -m cabal.sandbox.config) $pkg/
-  ln -s $(readlink -m .cabal.sandbox) $pkg/
-done
