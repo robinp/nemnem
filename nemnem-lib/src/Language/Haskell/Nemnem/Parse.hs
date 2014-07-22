9db798bb618aecc838f25420a95ccdd2e56adbc2
Robin Palotai <palotai.robin@gmail.com>
2014-07-22 21:43:48 +0000

    Started work to support better CPP (issue #6)

--- nemnem-lib/src/Language/Haskell/Nemnem/Parse.hs
+++ nemnem-lib/src/Language/Haskell/Nemnem/Parse.hs
@@ -29,14 +29,18 @@ data SourceInfo = SourceInfo
 -- | Returns also the parsed src on success.
 processModule
   :: (Monad m)
-  => [String]
+  => [(String, String)]
   -> SourceInfo
   -> String  -- ^ Source content
   -> StateT (Map MName ModuleInfo) m (Either String (String, ModuleInfo))
-processModule id_macro_prefixes SourceInfo{..} raw_src = StateT $ \modules ->
+processModule cpp_defines SourceInfo{..} raw_src = StateT $ \modules ->
   {-# SCC processModule #-} 
   let parsed_src = unTab raw_src
-      src = {-# SCC sourceTransform #-} unCpp id_macro_prefixes parsed_src
+      -- TODO: * exec new unCpp in IO
+      --       * in Parse/Module.hs, discard references that originate from
+      --         included files (hint: different filename in SrcInfo)
+      --       * and someone should skip references to/from cppExpanded lines
+      src = {-# SCC sourceTransform #-} parsed_src -- unCpp cpp_defines parsed_src
   in case {-# SCC hseParse #-} parse src of
        fail@(ParseFailed _ _) -> return (Left (show fail), modules)
        ParseOk (ast, comments) ->
@@ -89,59 +93,3 @@ unTab = TL.unpack . TL.replace tab spaces . TL.pack
   tab = TL.pack "\t"
   spaces = TL.pack (replicate 8 ' ')
 
--- | Offset-preserving removal of CPP-directives and possible macro calls.
--- TODO could support basic ifdefs like version checks.. but even nicer would be
---      a cpphs-like package which returns a range mapping between the original
---      and the transformed source, so the transformed could be analyzed, but
---      results would be marked up on the original.
-unCpp :: [String] -> String -> String
-unCpp id_macro_prefixes = {-# SCC unCpp #-}
-  unlines 
-  . reverse . trd
-  . foldl replaceCpp (False, False, [])
-  . lines
-  where
-  replaceCpp (was_where, inside_def, res) l0 = {-# SCC replaceCpp #-}
-    -- TODO emit preprocessor ranges for syntax highlight
-    --
-    -- If we didn't encounter the `where` keyword of the module, don't try to
-    -- strip macro-like calls since it can match export patterns.
-    let l = if was_where then removeMacroCall l0 else l0
-        ends_in_slash = "\\" `L.isSuffixOf` l
-        start_def = "#" `L.isPrefixOf` l
-        -- TODO this is pretty fragile, could look for `module X (.....) where`
-        new_was_where = was_where || "where" `L.isInfixOf` l
-    in if start_def || inside_def
-       then let replaced =
-                  -- Length-preserving transform.
-                  replicate (length l) ' '
-            in (new_was_where, ends_in_slash, replaced:res)
-       else (new_was_where, False, l:res)
-    where
-    -- | Heuristic to get rid of CHECK_BOUNDS(x,y,z) like invocations.
-    removeMacroCall :: String -> String
-    removeMacroCall l = {-# SCC removeMacroCall #-}
-      let matches = matchAll pattern l :: [MatchArray] -- Skip imports since an import `SomeModule (XY(a,b,c..))` would match
-      in if "import" `L.isPrefixOf` l || null matches then l
-         else let offset_and_lengths =
-                    map modifyOffsetForNestedParens
-                    . filter canRemoveMacroCall
-                    . concat . map (A.elems)
-                    $ matches
-                  regions = map (\(off,len) -> mkRange () off (off+len))
-                              offset_and_lengths
-              in transformRegions (\_ e -> replicate (length e) ' ') regions l
-      where
-      pattern = makeRegex ("[A-Z][A-Z0-9_]+\\([^)]*\\)" :: String)  :: Regex
-      canRemoveMacroCall (offset, _) =
-        let frag = takeWhile (/= '(') . drop offset $ l
-        in (`L.isPrefixOf` frag) `any` id_macro_prefixes
-      modifyOffsetForNestedParens (offset, _) =
-        let (name, rest) = L.break (== '(') . drop offset $ l
-        in (offset, length name + parlen 0 0 rest)
-      parlen i cnt xs = case (cnt, xs) of
-        (_, '(':rs) -> parlen (i+1) (cnt+1) rs
-        (1, ')':_)  -> i+1
-        (x, ')':rs) -> parlen (i+1) (x-1) rs
-        (x, _:rs)   -> parlen (i+1) x rs
-        _ -> i
---
+++ nemnem-lib/src/Language/Haskell/Nemnem/Parse/Cpp.hs
@@ -0,0 +1,58 @@
+{-# LANGUAGE RecordWildCards #-}
+module Language.Haskell.Nemnem.Parse.Cpp
+  ( CppLines(..)
+  , unCpp
+  ) where
+
+import Control.Monad
+import qualified Data.List as L
+import qualified Data.Set as S
+import Language.Preprocessor.Cpphs
+
+data CppLines = CppLines
+  { cppErased :: S.Set Int  -- ^ Note: could store as intervals, if worth it.
+  , cppExpanded :: S.Set Int
+  }
+  deriving (Show)
+
+-- | Pity that cpphs runs in IO instead of something more abstract.
+unCpp :: [(String, String)] -> String -> String -> IO (String, CppLines)
+unCpp defines src_name source = {-# SCC unCpp #-} do
+  let include_path = []
+      bool_opts = defaultBoolOptions { hashline = False }
+  transformed_src <- macroPass defines bool_opts
+                       <=< cppIfdef src_name defines include_path bool_opts
+                       $ source
+  let cleaned_lines = rollup src_name transformed_src
+      original_lines = lines source
+      cpp_lines = diffLines original_lines cleaned_lines
+  return (transformed_src, cpp_lines)
+
+rollup :: String -> String -> [String]
+rollup src_name src_with_line_pragmas = {-# SCC rollup #-}
+  let src_lines = lines src_with_line_pragmas
+  in go False src_lines []
+  where
+  go _ [] acc = reverse acc
+  go should_emit (l:ls) acc =
+    if "{-# LINE" `L.isPrefixOf` l then
+      let emit = src_name `L.isInfixOf` l
+      in go emit ls acc
+    else
+      let nacc = if should_emit then (l:acc) else acc
+      in go should_emit ls nacc
+
+diffLines :: [String] -> [String] -> CppLines
+diffLines orig cppd = {-# SCC diffLines #-}
+  go 1 orig cppd (CppLines S.empty S.empty)
+  where
+  go _ [] _ acc = acc
+  go _ _ [] acc = acc
+  go line (o:os) (p:ps) acc =
+    if o == p then
+      go (line+1) os ps acc
+    else
+      let nacc = if null p
+                 then acc { cppErased = S.insert line $ cppErased acc }
+                 else acc { cppExpanded = S.insert line $ cppExpanded acc }
+      in go (line+1) os ps nacc
